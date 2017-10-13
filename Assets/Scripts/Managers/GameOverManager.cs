3e3a52af08e46d67eb4371ece11a680948f504d8
Guilherme Galarça Dutra <udutra@gmail.com>
2017-10-13 10:37:25 -0300

    Assets
    Assets ok

---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public float restartDelay = 5f;
+
+    Animator anim;
+    float restartTimer;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (playerHealth.currentHealth <= 0)
+        {
+            // ... tell the animator the game is over.
+            anim.SetTrigger("GameOver");
+
+            // .. increment a timer to count up to restarting.
+            restartTimer += Time.deltaTime;
+        }
+    }
+   
+}
